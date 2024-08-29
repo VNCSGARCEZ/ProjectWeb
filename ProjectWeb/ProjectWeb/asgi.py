da0ee0120c717fe8876edbda3bd78b9d15dbe632
vncsg <vncsgarcez@gmail.com>
2024-08-28 21:53:56 -0300

    config de ambiente

---
+++ ProjectWeb/ProjectWeb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ProjectWeb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from dj_static import Cling, MediaCling
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectWeb.settings')
+
+application = Cling(MediaCling(get_asgi_application()))
